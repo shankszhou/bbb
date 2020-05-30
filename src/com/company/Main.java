1ab9987102e44bf6fe91dbfc28a646caa0bb9968
zhouyue <1440298847@qq.com>
2020-05-30 22:01:33 +0800

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,37 @@
+package com.company;
+
+import java.util.UUID;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*System.out.println("aaa");
+        int a = 1;
+        try {
+            if(a == 1){
+                throw new Two("aaa");
+            }
+        } catch (Two two) {
+            two.printStackTrace();
+        }*/
+
+        for (int i=0;i<10;i++) {
+            UUID uuid = UUID.randomUUID();
+            System.out.println("["+uuid+"]");
+        }
+
+    }
+
+    public void test(int a ){
+        if(a == 1){
+            throw new Two("aaa");
+        }
+    }
+}
+
+class Two extends RuntimeException {
+
+    public Two(String code){
+        super(code);
+    }
+}
